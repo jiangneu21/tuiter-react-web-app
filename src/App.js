4e6d8537abfa758861d378b7613f5b674ddb2546
xiaomengjiang <jiang.xiaom@northeastern.edu>
2023-01-20 14:53:16 -0500

    Add welcome message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Web Dev CS5610 Section 7
         </p>
         <a
           className="App-link"
