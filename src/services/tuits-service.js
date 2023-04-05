76fc05c4a7133fd19669e89f14fb60ccdf3fcf6b
xiaomengjiang <jiang.xiaom@northeastern.edu>
2023-04-05 03:44:15 -0400

    A9

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //const TUITS_API = 'https://tuiter-node-server-app-kt4k.onrender.com/api/tuits';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
-//
+
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
